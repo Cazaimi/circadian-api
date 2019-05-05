7d19b2944d0f5e3b31e37dfa8b460547d0eb263b
Anmol Shukla <anmol01shukla@gmail.com>
2019-05-05 15:04:23 +0530

    Initialized an empty express app. Also created a database schema.

---
+++ api/controllers/HourData.js
@@ -0,0 +1,19 @@
+module.exports = {
+    create (req, res, next) {
+      // Get data from req.
+
+      // 
+    },
+
+    read (req, res, next) {
+
+    },
+
+    delete (req, res, next) {
+
+    },
+
+    update (req, res, next) {
+
+    }
+}
